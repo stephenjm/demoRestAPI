83319dab15def7c1f0053654971f83142cefa313
stephenjm <54375213+stephenjm@users.noreply.github.com>
2019-08-27 22:04:58 +0100

    Update to add schema create query

--- conf/create.sql
+++ conf/create.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE `demoschema` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
 CREATE TABLE `demo_table` (
   `policyID` int(11) NOT NULL,
   `statecode` varchar(2) NOT NULL,
